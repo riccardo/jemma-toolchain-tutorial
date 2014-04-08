79399052dec12d0dd82d5f1f088b14e29da7d109
riccardo <riccardo.to@gmail.com>
2014-04-08 22:42:46 +0200

    non-working yet

--- jemma-toolchain-tutorial.guicommandsservice/src/test/java/org/energy_home/jemma/jemma_toolchain_tutorial/guicommandsservice/api/GUICommandsService.java
+++ jemma-toolchain-tutorial.guicommandsservice/src/test/java/org/energy_home/jemma/jemma_toolchain_tutorial/guicommandsservice/api/GUICommandsService.java
@@ -1,9 +1,21 @@
 package org.energy_home.jemma.jemma_toolchain_tutorial.guicommandsservice.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/")
 public interface GUICommandsService {
-	
+
+	//FIXME maybe I can try out a PUT here
 	public void Button1Pressed(int button_id);
+
+	//FIXME maybe I can try out a PUT here	
 	public void gaugechanged(double value);
+
+	@GET
+	@Path("/gaugelevel")
+	@Produces("application/json")
 	public double getCurrentGaugeLevel();
 
 }
