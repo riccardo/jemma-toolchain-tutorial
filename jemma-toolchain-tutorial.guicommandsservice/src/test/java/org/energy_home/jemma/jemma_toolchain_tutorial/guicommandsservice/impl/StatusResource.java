2a87b751aa33e83de10d20dd3e2cd23327917c66
riccardo <tomasi@ismb.it>
2014-05-09 15:27:11 +0200

    working example Jersey

--- jemma-toolchain-tutorial.guicommandsservice/src/test/java/org/energy_home/jemma/jemma_toolchain_tutorial/guicommandsservice/impl/StatusResource.java
+++ jemma-toolchain-tutorial.guicommandsservice/src/test/java/org/energy_home/jemma/jemma_toolchain_tutorial/guicommandsservice/impl/StatusResource.java
@@ -3,14 +3,15 @@ package org.energy_home.jemma.jemma_toolchain_tutorial.guicommandsservice.impl;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("status")
 public class StatusResource {
 
-    @GET @Produces("text/plain")
-    public String getStatus() {
-        //return new MyComplexResource();
-    	return "active";
+    @GET @Produces(MediaType.APPLICATION_JSON)
+    public MyComplexResource getStatus() {
+        return new MyComplexResource();
+    	//return "active";
     }
 
 }
